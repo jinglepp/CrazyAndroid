8643f332268ed922bcdaf8c06cd99aa85950cefb
jinglepp <jinglepp@qq.com>
2017-04-24 17:36:25 +0800

    create project

---
+++ 跟随手指的小球/src/main/java/com/example/ball/DrawCircleView.java
@@ -0,0 +1,16 @@
+package com.example.ball;
+
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+
+public class DrawCircleView extends View {
+
+    public DrawCircleView(Context context) {
+        super(context);
+    }
+}
