3b666fcc5a056c14afc963595b21faec6d16889a
jinglepp <jinglepp@qq.com>
2017-04-25 11:11:25 +0800

    create project

--- 跟随手指的小球/src/main/java/com/example/ball/DrawCircleView.java
+++ 跟随手指的小球/src/main/java/com/example/ball/DrawCircleView.java
@@ -2,15 +2,40 @@ package com.example.ball;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-/**
- * Created by Administrator on 2017/4/24.
- */
-
 public class DrawCircleView extends View {
+    public float currentX = 40;
+    public float currentY = 50;
+    Paint p = new Paint();//定义画笔
 
     public DrawCircleView(Context context) {
         super(context);
+    }
+
+    public DrawCircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        p.setColor(Color.RED);
+        canvas.drawCircle(currentX, currentY, 15, p);
+    }
+
+    //重写触摸事件
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        currentX = event.getX();
+        currentY = event.getY();
+        invalidate();//通知重绘当前组件
+        return true;//true表示方法已经处理该事件
     }
 }
--- 跟随手指的小球/src/main/java/com/example/ball/MainActivity.java
+++ 跟随手指的小球/src/main/java/com/example/ball/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        LinearLayout root = (LinearLayout) findViewById(R.id.root);
+//        final DrawView draw = new DrawView(this);
+//        //设置自定义组件的最小宽度,高度
+//        draw.setMinimumWidth(300);
+//        draw.setMinimumHeight(300);
+//        root.addView(draw);
     }
-
 }
