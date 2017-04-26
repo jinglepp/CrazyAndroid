2c9e6dafe7bd1b2350e7764af1dee548088d516e
jinglepp <jinglepp@qq.com>
2017-04-26 17:56:46 +0800

    create project

--- textviewdemo/src/main/java/com/example/textviewdemo/MainActivity.java
+++ textviewdemo/src/main/java/com/example/textviewdemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.textviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckedTextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.custum_textview);
+    }
+
+    public void onClick(View view) {
+        Log.d("LEE", "onClick");
+        CheckedTextView ctv = (CheckedTextView) view;
+        ctv.toggle();
+        if (ctv.isChecked()) {
+            ctv.setTextColor(Color.parseColor("#FF4081"));
+            ctv.setCheckMarkDrawable(R.drawable.ok);
+        } else {
+            ctv.setTextColor(Color.GRAY);
+            ctv.setCheckMarkDrawable(null);
+        }
     }
 }
